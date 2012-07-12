a93d1ac357e8e5e6f7aa723c22fdfb167f4639e8
John Jacobsen <john@mail.npxdesigns.com>
2012-07-12 15:27:06 +0200

    Successfully decoding file index, proceeding to sequence records

--- src/jenome/core.clj
+++ src/jenome/core.clj
@@ -11,32 +11,38 @@ http://genome.ucsc.edu/FAQ/FAQformat#format7
 
 (gl/defcodec hdr-codec [:uint32-le :uint32-le :uint32-le :uint32-le])
 (gl/defcodec byte-codec [:byte])
-(gl/defcodec bc2 [:byte :byte :byte :byte])
+(gl/defcodec bc2 (gl/repeated :byte) );[:byte :byte :byte :byte])
 (gl/defcodec seq-hdr-offset :uint32-le)
 
-(defn get-bytes [n inf]
+(defn get-bytes
+  [n inf]
   (let [buf (byte-array n)]
     (.read inf buf)
     buf))
 
-(defn sequence-count [infile]
+(defn sequence-count
+  [infile]
   (let [hdr-bytes (get-bytes 16 infile)
-        [signature version
-         sequence-count reserved] (glio/decode hdr-codec hdr-bytes)]
+        [signature ver cnt reserved] (glio/decode hdr-codec hdr-bytes)]
   (assert (= signature 0x1A412743))
-  (assert (= version reserved 0))
-  sequence-count))
+  (assert (= ver reserved 0))
+  cnt))
 
-(defn get-seq-header [infile]
+(defn bytes-to-str
+  [bytes]
+  (apply str (map (comp char int) bytes)))
+
+(defn get-seq-header
+  [infile]
   (let [name-len-byte (get-bytes 1 infile)
         name-len (first (glio/decode byte-codec name-len-byte))
-        namebytes (glio/decode bc2 (get-bytes name-len infile))
-        seqname (apply str (map (comp char int) namebytes))
+        seqname (bytes-to-str (get-bytes name-len infile))
         offset (glio/decode seq-hdr-offset (get-bytes 4 infile))]
-    {:name seqname :offset offset}))
+    [seqname offset]))
 
 (let [fname "/Users/jacobsen/Programming/Lisp/Clojure/jenome/hg19.2bit"
       infile (io/input-stream fname)
       seqcount (sequence-count infile)
-      {:keys [name offset]} (get-seq-header infile)
-      _ (println name offset)])
+      index (for [_ (range seqcount)]
+              (get-seq-header infile))]
+  (apply str (interpose " " (map first index))))
