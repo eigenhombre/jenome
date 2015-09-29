e8a7b1695afa4f06a7fcc4989788b976d8f2612c
John Jacobsen <john@mail.npxdesigns.com>
2015-09-28 22:04:59 -0500

    Finish excising Midje.

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
 (defproject jenome "1.0.0-SNAPSHOT"
   :description "Hacking around with the human genome"
   :main jenome.core
-  :profiles {:dev {:dependencies [[midje "1.7.0"]
-                                  [speclj "3.3.0"]]
-                   :plugins [[lein-midje "3.1.3"]
-                             [speclj "3.3.0"]]}}
+  :profiles {:dev {:dependencies [[speclj "3.3.0"]]
+                   :plugins [[speclj "3.3.0"]]}}
   :test-paths ["spec"]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
--- spec/jenome/decode_spec.clj
+++ spec/jenome/decode_spec.clj
@@ -1,6 +1,7 @@
 (ns jenome.decode-spec
   (:require [clojure.java.io :refer [as-file resource]]
             [jenome.decode :refer :all]
+            [jenome.util :refer [monotonic?]]
             [speclj.core :refer :all]))
 
 
@@ -36,3 +37,37 @@
     (should= (yeast-section 1 12) "CACACCACAC")
     (should= (yeast-section 10000 10011) "GAATGAATCG")
     (should= (yeast-section 100000 100011) "GGTATTATTT")))
+
+
+(describe "yeast metadata"
+  (it "decodes correctly"
+    (let [hdrs(->> (resource "sacCer3.2bit")
+                   as-file
+                   sequence-headers)]
+      (should= (count hdrs) 17)
+      (should (monotonic? (map :dna-offset hdrs)))
+      (should= (map :mask-block-count hdrs) (repeat 17 0))
+      (should= (map :n-block-sizes hdrs) (repeat 17 ())))))
+
+
+;; FIXME: overly specific, low-level test?
+(describe "determination of read blocks"
+  (it "..."
+    (should= (get-buffer-starts-and-lengths 0 1000 1000) [[0 1000]])
+    (should= (get-buffer-starts-and-lengths 0 1000 1001) [[0 1000] [1000 1]])
+    (should= (get-buffer-starts-and-lengths 100 200 512) [[100 200] [300 200]
+                                                          [500 112]])))
+
+
+(describe "is-in-an-n-block"
+  (it "determines n-blocks correctly"
+    (let [n-block-starts [0 1000]
+          n-block-lengths [3 1]]
+      (should-not (is-in-an-n-block   -1 n-block-starts n-block-lengths))
+      (should     (is-in-an-n-block    0 n-block-starts n-block-lengths))
+      (should     (is-in-an-n-block    1 n-block-starts n-block-lengths))
+      (should     (is-in-an-n-block    2 n-block-starts n-block-lengths))
+      (should-not (is-in-an-n-block    3 n-block-starts n-block-lengths))
+      (should-not (is-in-an-n-block  999 n-block-starts n-block-lengths))
+      (should     (is-in-an-n-block 1000 n-block-starts n-block-lengths))
+      (should-not (is-in-an-n-block 1001 n-block-starts n-block-lengths)))))
